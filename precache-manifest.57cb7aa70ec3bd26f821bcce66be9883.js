631b814d32c3c89791e32b26e0ae80f4acb1e808
Volt Security <voltsecurity@gmail.com>
2020-06-23 15:35:28 +1200

    Updates

---
+++ precache-manifest.57cb7aa70ec3bd26f821bcce66be9883.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4212fb1cc1734681fe4e5b7bdb3562e",
+    "url": "/asset-manager/index.html"
+  },
+  {
+    "revision": "8eb1f85b231d885351f7",
+    "url": "/asset-manager/static/css/main.b3de2453.chunk.css"
+  },
+  {
+    "revision": "c39b6b8fee4453689ba5",
+    "url": "/asset-manager/static/js/2.ef715418.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/asset-manager/static/js/2.ef715418.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eb1f85b231d885351f7",
+    "url": "/asset-manager/static/js/main.b0938315.chunk.js"
+  },
+  {
+    "revision": "e81b8cbe4b970bcfdbd4",
+    "url": "/asset-manager/static/js/runtime-main.89462d6c.js"
+  },
+  {
+    "revision": "b0b36b5bf8df03a1d924cb0d21dee6b9",
+    "url": "/asset-manager/static/media/drill.b0b36b5b.jpg"
+  },
+  {
+    "revision": "07aba8e5d2c17fda4d13064e582bdc43",
+    "url": "/asset-manager/static/media/generator.07aba8e5.jpg"
+  },
+  {
+    "revision": "8d76d8c5dffbb028ceaaae09bee0d4b7",
+    "url": "/asset-manager/static/media/ladder.8d76d8c5.jpg"
+  },
+  {
+    "revision": "8470ec52cf5eb3e3f6a88d7f1e4d1a56",
+    "url": "/asset-manager/static/media/snake.8470ec52.jpg"
+  }
+]);
