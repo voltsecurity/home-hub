1a78860e55855d14e2c3807449d49accf3cadab3
Volt Security <voltsecurity@gmail.com>
2020-08-01 23:37:52 +1200

    Updates

---
+++ precache-manifest.578cd111579023c291803447f9d059f8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f98b2cbecf8e77a18097d72207cc5a9d",
+    "url": "/asset-manager/index.html"
+  },
+  {
+    "revision": "96f7a9386324de50c471",
+    "url": "/asset-manager/static/css/main.aa1f1f56.chunk.css"
+  },
+  {
+    "revision": "77b3e05fefa618ec627c",
+    "url": "/asset-manager/static/js/2.c7184429.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/asset-manager/static/js/2.c7184429.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96f7a9386324de50c471",
+    "url": "/asset-manager/static/js/main.de8b15b5.chunk.js"
+  },
+  {
+    "revision": "e81b8cbe4b970bcfdbd4",
+    "url": "/asset-manager/static/js/runtime-main.89462d6c.js"
+  },
+  {
+    "revision": "0fa840e78f0c3ff906a1486c05d3376a",
+    "url": "/asset-manager/static/media/hikvision1.0fa840e7.jpeg"
+  },
+  {
+    "revision": "5259551b6f3c1db1862706cbeec5e43c",
+    "url": "/asset-manager/static/media/hikvision3.5259551b.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/asset-manager/assets/assetlist/precache-manifest.465e46f211fb58412fc429eeda708023.js"
+  "/asset-manager/precache-manifest.578cd111579023c291803447f9d059f8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/asset-manager/assets/assetlist/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/asset-manager/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
