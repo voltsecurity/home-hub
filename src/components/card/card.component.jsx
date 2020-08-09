1741c9dd539f2540a8a50e934aaca5d784bfd1a6
Volt Security <voltsecurity@gmail.com>
2020-08-09 22:37:43 +1200

    update

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,31 +1,21 @@
 import React from 'react';
-import Draggable from 'react-draggable';
 
 import './card.styles.scss';
 // import { withRouter } from 'react-router-dom';
 
-const Card = ({ item, id, handleClick, handleMenu }) => {
-    const nodeRef = React.useRef(null);
+const Card = ({ item, id, handleClick }) => (
+    <div id={id} className='card' onClick={() => handleClick(item)}>
+        <div className='card-image-container'>
+            <img className='card-image' src={item.imageUrl} alt="item" />
+        </div>
+        <div className='card-field-container'>
+            <p className='card-id'>{item.cameraID}</p>
+            <p className='card-field-one'>{item.model}</p>
+            <p className='card-field-two'>{item.brand}</p>
+            <p className='card-field-three'>{item.type}</p>
+        </div>
+    </div>
 
-    return (
-        <Draggable nodeRef={nodeRef}>
-            {/* <div id={id} className='card' onClick={() => handleClick(item)}> */}
-            <div ref={nodeRef} id={id} className='card' onContextMenu={handleMenu}>
-                <div className='card-image-container'>
-                    <img className='card-image' src={item.imageUrl} alt="item" />
-                </div>
-                <div className='card-field-container'>
-                    <p className='card-id'>{item.cameraID}</p>
-                    <p className='card-field-one'>{item.model}</p>
-                    {/* <p className='card-field-one title'><strong>Brand</strong></p> */}
-                    <p className='card-field-two'>{item.brand}</p>
-                    {/* <p className='card-field-two title'><strong>Type</strong></p> */}
-                    <p className='card-field-three'>{item.type}</p>
-                </div>
-                {/* <p>{item.model}</p> */}
-            </div>
-        </Draggable>
-    )
-}
+)
 
 export default Card;
